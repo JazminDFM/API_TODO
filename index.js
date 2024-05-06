148610a1dbfac29c34a4152b91fb45aa4e310de5
Jazmin <jazminmattachavez@gmail.com>
2024-05-06 14:41:29 +0200

    Creacion endpoint GET

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const {tareas} = require("./db");
 
 const express = require("express");
 
@@ -9,6 +10,21 @@ if(process.env.LOCAL){ //Funciona si ponemos LOCAL=true
     servidor.use(express.static("./pruebas"));
 }
 
+servidor.get("/tareas", async (peticion, respuesta) => {
+    try{
+        let tareas = await tareas();
 
+        respuesta.json(tareas);
+
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json(error);
+    }
+});
+
+servidor.use((peticion, respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado"});
+});
 
 servidor.listen(process.env.PORT);
