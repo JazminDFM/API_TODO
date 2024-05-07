fd45f8ec5cbd70ba7954e8844a095face8da2d29
Jazmin <jazminmattachavez@gmail.com>
2024-05-07 20:44:04 +0200

    Endpoint PUT hecho :D

--- index.js
+++ index.js
@@ -42,13 +42,40 @@ servidor.post("/tareas/nueva", async (peticion, respuesta, siguiente) => {
 
 servidor.delete("/tareas/borrar/:id([0-9]+)", async (peticion, respuesta) => {
     try{
-        let count = await borrarTarea(peticion.params.count);
+        let count = await borrarTarea(peticion.params.id);
         
         respuesta.json({ resultado : count ? "ok" : "ko" });
 
     }catch(error){
         respuesta.status(500);
         respuesta.json(error);
+    }
+});
+
+servidor.put("/tareas/actualizar/:id([0-9]+)/:operacion([1-2]+)", async (peticion, respuesta, siguiente) => {
+    if(peticion.params.operacion == 1){
+        if(!peticion.body.tarea || peticion.body.tarea.trim() == ""){
+            return siguiente(true);
+        }
+        try{
+            let edicionTexto = await editarTexto(peticion.params.id, peticion.body.tarea);
+    
+            respuesta.json(edicionTexto);
+    
+        }catch(error){
+            respuesta.status(500);
+            respuesta.json(error);
+        }
+    }else{
+        try{
+            let toggle = await toggleEstado(peticion.params.id);
+    
+            respuesta.json(toggle);
+    
+        }catch(error){
+            respuesta.status(500);
+            respuesta.json(error);
+        }
     }
 });
 
